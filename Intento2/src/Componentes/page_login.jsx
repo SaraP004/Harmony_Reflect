994979618f1e58a0c621439c4a496c9cd37e0cd9
Antonella Jativa <aljativa@puce.edu.ec>
2024-07-07 18:32:55 -0500

    informacion alamacenada en login y create

--- Intento2/src/Componentes/page_login.jsx
+++ Intento2/src/Componentes/page_login.jsx
@@ -1,37 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginstyle from '../styles/page_login.module.css';
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+  const [message, setMessage] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = credentials;
+
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || {};
+
+    if (existingUsers[username] && existingUsers[username].password === password) {
+      setMessage('Inicio de sesión exitoso.');
+      setShowMessage(true);
+    } else {
+      setMessage('Nombre de usuario o contraseña incorrectos.');
+      setShowMessage(true);
+    }
+  };
+
+  const closeMessage = () => {
+    setShowMessage(false);
+    if (message === 'Inicio de sesión exitoso.') {
+      window.location.href = '/game';
+    }
+  };
+
   return (
     <div className={loginstyle.body}>
-    <div className={loginstyle.container}>
-      <div className={loginstyle.imagen}>
-        <img id={loginstyle.registra_img} src="img/LOGIN.png" alt="Login" />
-      </div>
-      <div className={loginstyle.space}>
-        <div className={loginstyle.form}>
-          <div className={loginstyle.tittle}>
-            <b className={loginstyle.headline}>¡Hola! ¡Qué bueno es verte por aquí!</b>
+      {showMessage && (
+        <div className={loginstyle.overlay}>
+          <div className={loginstyle.messageBox}>
+            <p>{message}</p>
+            <button className={loginstyle.closeButton} onClick={closeMessage}>Aceptar</button>
           </div>
-          <div className={loginstyle.labels}>
-            <label htmlFor={loginstyle.username}>Nombre de Usuario o Correo</label>
-            <input type="text" id={loginstyle.username} name="username" />
-            <label htmlFor={loginstyle.password}>Contraseña</label>
-            <input type="text" id={loginstyle.password} name="password" />
-          </div>
-          <div className={loginstyle.buttons}>
-            <button className={loginstyle.button} id={loginstyle.CancelButton} onClick={() => window.location.href='/'}> 
-              <img src="" alt="" />
-              Cancelar
-            </button>
-            <button className={loginstyle.button} id={loginstyle.AcceptButton} onClick={() => window.location.href='/game'}> 
-              <img src="" alt="" />
-              Iniciar
-            </button>
+        </div>
+      )}
+      <div className={loginstyle.container}>
+        <div className={loginstyle.imagen}>
+          <img id={loginstyle.registra_img} src="/img/LOGIN.png" alt="Login" />
+        </div>
+        <div className={loginstyle.space}>
+          <div className={loginstyle.form}>
+            <form onSubmit={handleSubmit}>
+              <div className={loginstyle.tittle}>
+                <b className={loginstyle.headline}>¡Hola! ¡Qué bueno es verte por aquí!</b>
+              </div>
+              <div className={loginstyle.labels}>
+                <label htmlFor="username">Nombre de Usuario o Correo</label>
+                <input type="text" id="username" name="username" value={credentials.username} onChange={handleChange} required />
+                <label htmlFor="password">Contraseña</label>
+                <input type="password" id="password" name="password" value={credentials.password} onChange={handleChange} required />
+              </div>
+              <div className={loginstyle.buttons}>
+                <button className={loginstyle.button} id={loginstyle.CancelButton} type="button" onClick={() => window.location.href = '/'}>
+                  Cancelar
+                </button>
+                <button className={loginstyle.button} id={loginstyle.AcceptButton} type="submit">
+                  Iniciar
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
