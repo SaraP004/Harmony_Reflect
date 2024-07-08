19bd9ed07023a952d21e9538b22e791bbfd83d82
SaraPiedraPUCE <smpiedra@puce.edu.ec>
2024-07-07 19:23:56 -0500

    Ingreso de pagina para fondos de pantalla

---
+++ Intento2/src/Componentes/Wallpapers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Wallpapers.module.css';
+const Wallpapers = () => {
+    return(
+        <div className={styles.body}>
+            <div className={styles.container}>
+                <p>Aqui van los fondos</p>
+            </div>
+        </div>
+    );
+};
+export default Wallpapers;
+//{}
