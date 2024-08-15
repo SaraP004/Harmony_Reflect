a4c5c112dd2b965085b1a8bfa838306414edf3bf
Cristopher Puma <capuma@puce.edu.ec>
2024-08-14 22:58:25 -0500

    Conexión usuario-personje, back+front

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { crearUsuario, loginUsuario } from '../controller/userController.js';
-
 const router = Router();
 
 router.post('/create', crearUsuario);
 router.post('/login', loginUsuario);
-
 
 export default router;
